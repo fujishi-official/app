44c98e4486b7699e64809bef7a4f8bb446698d0a
Tomoya Fujishiro <fastsoda.bros@gmail.com>
2019-10-17 13:17:07 +0900

    chapter3 comp

---
+++ app/confirm.php
@@ -0,0 +1,22 @@
+<?php
+// postdate
+$user_name = $_POST['user_name'];
+$hobby = $_POST['hobby'];
+?>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+    <h1>受信ページ</h1>
+    <p>あなたの名前は <?php echo $user_name; ?> さんです</p>
+    <p>趣味は <?php echo $hobby; ?> です</p>
+    <p>こちらの情報でよろしいですか？</p>
+    <!-- form -->
+    <form action="complete.php" method='POST'>
+    <input type="hidden" name="user_name" value="<?php echo $user_name; ?>">
+    <input type="hidden" name="hobby" value="<?php echo $hobby; ?>">
+    <input type="submit" value="登録">
+    </form>
+</body>
+</html>
---
+++ app/send.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+    <h1>練習フォーム</h1>
+    <p>次のページにデータを渡す。</p>
+    <!-- form -->
+    <form action="./confirm.php" method='POST'>
+    <label>名前</label>
+    <input type="text" name="user_name">
+    <label>趣味</label>
+    <input type="text" name="hobby">
+    <input type="submit" value="確認する">
+    </form>
+</body>
+</html>
